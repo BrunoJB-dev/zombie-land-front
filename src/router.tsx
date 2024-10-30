bced61dcce786f9e4485b1c13a894b6bbebfca02
Laura-Lacoste <lacostelaura@live.fr>
2024-10-30 11:07:01 +0100

    feat: axios + list attraction start

--- src/router.tsx
+++ src/router.tsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from 'react-router-dom';
 import MainLayout from './layouts/MainLayout';
 import HomePage from './pages/HomePage';
+import AttractionListPage from './pages/AttractionList/AttractionList';
 
 const router = createBrowserRouter([
   {
@@ -10,6 +11,10 @@ const router = createBrowserRouter([
       {
         path: '/',
         element: <HomePage />,
+      },
+      {
+        path: '/attractions',
+        element: <AttractionListPage />,
       },
     ],
   },
