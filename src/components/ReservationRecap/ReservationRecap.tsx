234c5d373f7349a22b37e974757a3cdf4063d8a6
Laura-Lacoste <lacostelaura@live.fr>
2024-10-31 15:37:18 +0100

    Feat : Reservation Get Css

--- src/components/ReservationRecap/ReservationRecap.tsx
+++ src/components/ReservationRecap/ReservationRecap.tsx
@@ -1,9 +1,38 @@
-import type { Reservation } from "../../@types/reservation";
+import "./ReservationRecap.scss"
 
-function ReservationRecap (){
+type ReservationRecapProps = {
+  startDate: Date | null;
+  number: number;
+};
+
+function ReservationRecap ({startDate, number}: ReservationRecapProps){
+
+    const formatedDate = startDate ? startDate.toLocaleDateString('fr-CA') : "Date non sélectionnée";
+    const price = number * 60;
+
   return(
-    <div>
-      Je serai le récap
+    <div className="reservation-recap">
+      <div className="reservation-border">
+        <div className="reservation-head">
+          <img src="../../../public/favicon.jpg" alt="" />
+          <div>
+            <p className="reservation-billet">{number} Billet(s) Daté(s) 1 jours</p>
+            <p className="reservation-date">Valable le {formatedDate}</p>
+            <p>Annulation possible jusqu'à 10 jours</p>
+          </div>
+        </div>
+        <div className="reservation-content">
+          <div className="reservation-price">
+            <p>{number} x</p>
+            <p className="space"> </p>
+            <p>60 €</p>
+          </div>
+          <div className="reservation-total">
+            <p className="totale-text">TOTAL</p>
+            <p className="total-price">{price} €</p>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
--- src/pages/Reservation/ReservationPage.tsx
+++ src/pages/Reservation/ReservationPage.tsx
@@ -3,15 +3,19 @@ import { instanceAxios } from '../../utils/axios.ts';
 import ReservationCalendar from '../../components/ReservationCalendar/ReservationCalendar.tsx';
 import ReservationRecap from '../../components/ReservationRecap/ReservationRecap.tsx';
 
+import "./ReservationPage.scss"
+
 function ReservationPage() {
+  const [startDate, setStartDate] = useState<Date | null>(null);
+  const [number, setNumber] = useState<number>(0);
   return (
-    <main>
-      <div>
+    <main className='reservation'>
+      <div className='banner-reservation'>
         <h1>Réservation</h1>
       </div>
-      <h2>Achetez vos billets pour zombieland</h2>
-      < ReservationCalendar />
-      < ReservationRecap />
+      <h2 className='title'>Achetez vos billets pour zombieland</h2>
+      < ReservationCalendar startDate={startDate} setStartDate={setStartDate} number={number}  setNumber={setNumber}/>
+      < ReservationRecap  startDate={startDate} number={number}/>
     </main>
   );
 }
