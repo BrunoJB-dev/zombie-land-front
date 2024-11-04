d0c1ecc9941faf21f86c1f9512b32067c2a70761
Mickaël-Goua <micke.goua@gmail.com>
2024-11-04 17:58:38 +0100

    feat: detail page

--- src/components/AttractionDetail/AttractionDetail.tsx
+++ src/components/AttractionDetail/AttractionDetail.tsx
@@ -1,4 +1,6 @@
 import type { Attraction } from '../../@types/attraction';
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from 'react-responsive-carousel';
 
 import "./AttractionDetail.scss"
 
@@ -9,26 +11,61 @@ type AttractionListProps = {
 function AttractionDetail({ detail }: AttractionListProps) {
   return (
     <div>
-      <div className='banner-datail'>
-          <h1>{detail.name}</h1>
+      <div className='banner-detail'>
+         <h1>{detail.name}</h1>
       </div>
 
       <div className='div-article'>
-        <div>
-          <p>{detail.description_short}</p>
+        <div className='article-img'>         
           <p>{detail.description_long}</p>
+          <Carousel className='img'        
+            showThumbs={true}
+            showStatus={false}
+            infiniteLoop
+            stopOnHover>
+            <div>
+              <img src={`http://localhost:3000/${detail.image}`} alt={detail.name} />
+            </div>
+            <div>
+              <img src={`http://localhost:3000/${detail.caroussel1}`} alt={detail.name} />
+            </div>
+            <div>
+              <img src={`http://localhost:3000/${detail.caroussel2}`} alt={detail.name} />
+            </div>
+            <div>
+              <img src={`http://localhost:3000/${detail.caroussel3}`} alt={detail.name} />
+            </div>
+          </Carousel>
+          
         </div>
 
-          <img className='img' src={`http://localhost:3000/${detail.image}`} alt={detail.name} />
+        <p>{detail.description_short}</p>
       </div>
 
-        <div className='article-img'>
-          <p>{detail.closing_time}</p>
-          <p>{detail.height_restriction} cm</p>
-          <p>{detail.disable_access}</p>
-          <p>{detail.weather_hazard}</p>
-          <p>{detail.health_hazard}</p>
+    <div className='div-test'>
+        <div className='detail-icon'>
+          <div>
+            <i className="fa-solid fa-clock fa-2xl"/>
+            <p>{detail.closing_time}</p>
+          </div>
+          <div>
+            <i className="fa-solid fa-restroom fa-2xl"/>
+            <p>{detail.height_restriction} cm</p>
+          </div>
+          <div>
+            <i className="fa-brands fa-accessible-icon fa-2xl"/>
+            <p>{detail.disable_access}</p>
+          </div>
+          <div>
+            <i className="fa-solid fa-cloud-sun fa-2xl"/>
+            <p>{detail.weather_hazard}</p>
+          </div>
+          <div>
+            <i className="fa-solid fa-notes-medical fa-2xl"/>
+            <p>{detail.health_hazard}</p>
+          </div>
         </div>
+    </div>
 
       
     </div>
