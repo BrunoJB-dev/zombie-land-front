8980600caaac7f05454d94f2e58b60e8bd405d9d
BrunoJB <bruno.jean-baptiste@oclock.school>
2024-11-06 22:26:06 +0100

    feat: homepage-modal

---
+++ src/components/InfoPlan/InfoPlan.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import './InfoPlan.scss';
+
+function InfoPlan() {
+  return (
+    <div className="modal-content">
+      <h3 className="modal-title">Comment nous rejoindre ?</h3>
+      
+    </div>
+  )
+}
+
+export default InfoPlan;
