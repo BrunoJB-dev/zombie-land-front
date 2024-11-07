bf8fd8b904d2ad36514bfc222c668787f51dbe0e
BrunoJB <bruno.jean-baptiste@oclock.school>
2024-11-07 22:38:28 +0100

    feat: profilBtnOnOff-logoutBtnOnOff

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,5 +1,6 @@
 import { Link } from "react-router-dom";
 import { useState } from 'react';
+import { useAuth } from "../../AuthContext.tsx";
 
 import "./Header.scss";
 import SignUpForm from "../SignupForm/SignupForm.tsx";
@@ -9,25 +10,36 @@ import Modal from "../Modal/Modal.tsx";
 
 function Header() {
   const [isModalOpen, setIsModalOpen] = useState(false);
-  
-    return (
-  <div className="header">
-    <Link className="logo" to ='/'><img src="../../public/logo-zombieland.png" alt="logo zombieland" /></Link>
-    <nav>
-      <Link to="/">Accueil</Link>
-      <Link to="/attractions">Découvrez les attractions</Link>
-      <Link to="/profile">Mon compte</Link>
-    </nav>
-    <div className="icons">
-      <i className="fa-solid fa-magnifying-glass fa-2xl"/>
-      <button type="button" className="user-button" onClick={() => setIsModalOpen(true)}><i className="fa-solid fa-user fa-2xl"/></button>
-      <Modal isOpen={isModalOpen} onClose={() => setIsModalOpen(false)}>
+  const { token, logout } = useAuth(); // Récupération du token et de la fonction logout
+
+  return (
+    <div className="header">
+      <Link className="logo" to='/'><img src="../../public/logo-zombieland.png" alt="logo zombieland" /></Link>
+      <nav>
+        <Link to="/">Accueil</Link>
+        <Link to="/attractions">Découvrez les attractions</Link>
+        {token && <Link to="/profile">Mon compte</Link>}
+      </nav>
+      <div className="icons">
+        <i className="fa-solid fa-magnifying-glass fa-2xl" />
+        <button type="button" className="user-button" onClick={() => setIsModalOpen(true)}>
+          <i className="fa-solid fa-user fa-2xl" />
+        </button>
+        <Modal isOpen={isModalOpen} onClose={() => setIsModalOpen(false)}>
           <SignUpForm />
           <LoginForm />
-      </Modal>
+        </Modal>
+      </div>
+      <Link className="booking" to="/reservation">Réserver</Link>
+
+      {/* Bouton de déconnexion qui s'affiche uniquement si l'utilisateur est connecté */}
+      {token && (
+        <button type="button" className="logout-button" onClick={logout}>
+          Déconnexion
+        </button>
+      )}
     </div>
-    <Link className="booking" to="/reservation">Réserver</Link>
-  </div>
   );
 }
+
 export default Header;
