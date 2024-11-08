07f8f8d1b2cf307667fb8c1dc0290ec6420f2132
Mickaël-Goua <micke.goua@gmail.com>
2024-11-08 17:07:17 +0100

    feat: update user

--- src/components/myProfil/myProfil.tsx
+++ src/components/myProfil/myProfil.tsx
@@ -8,18 +8,19 @@ import "./myProfil.scss"
 function myProfile() {
 
   const [selectedAvatar, setSelectedAvatar] = useState(avatarFemme);
-  const [user, setUser] = useState<User>()
-
+  const [userId, setUserId] = useState<User>()
+  
   useEffect(()=> {
     instanceAxios.get('/api/profile').then(({data}) => {
-      setUser(data)   
+      setUserId(data)   
     })
   }, []);
 
-  // Fonction pour gérer le changement d'avatar
+  
   const handleAvatarChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
-    setSelectedAvatar(event.target.value);
+      setSelectedAvatar(event.target.value);
   };
+   
 
   return(
     <div className="myProfile">
@@ -32,31 +33,31 @@ function myProfile() {
         <div className='separation-label'>
           <div>
             <label htmlFor="firstname">Nom</label>
-            <input type="text" id="firstname" placeholder="Nom*" defaultValue={user?.lastname}/>        
+            <input type="text" id="firstname" placeholder="Nom*" defaultValue={userId?.lastname} />        
           </div>
           <div>
             <label htmlFor="lastname">Prénom</label>
-            <input type="text" id="lastname" placeholder="Prénom*"defaultValue={user?.firstname}/>
+            <input type="text" id="lastname" placeholder="Prénom*"defaultValue={userId?.firstname} />
           </div>
           <div>
             <label htmlFor="pseudo">Pseudo</label>
-            <input type="text" id="pseudo" placeholder="Pseudo*"/>
+            <input type="text" id="pseudo" placeholder="Pseudo*"defaultValue={userId?.pseudo} />
           </div>
           <div>
             <label htmlFor="mail">Mail</label>
-            <input type="text" id="mail" placeholder="Adresse email*"defaultValue={user?.email}/>
+            <input type="text" id="mail" placeholder="Adresse email*"defaultValue={userId?.email} />
           </div>
           <div>
             <label htmlFor="address">Adresse</label>
-            <input type="text" id="address" placeholder="Adresse*"/>
+            <input type="text" id="address" placeholder="Adresse*"defaultValue={userId?.adress} />
           </div>
           <div>
             <label htmlFor="postal_code">CP</label>
-            <input type="text" id="postal_code" placeholder="Code postal*"/>
+            <input type="text" id="postal_code" placeholder="Code postal*"defaultValue={userId?.postal_code} />
           </div>
           <div>
             <label htmlFor="country">Ville</label>
-            <input type="text" id="country" placeholder="Ville*"/>
+            <input type="text" id="country" placeholder="Ville*"defaultValue={userId?.country} />
           </div>
         </div>
           
@@ -71,10 +72,11 @@ function myProfile() {
 
       </div>
       <div className="button-profile">
-        <button className="button-style" type="button">Modifier mes informations</button>
+        <button className="button-style" type="button" >Modifier mes informations</button>
       </div>
     </div>
   )
 }
 
 export default myProfile;
+
