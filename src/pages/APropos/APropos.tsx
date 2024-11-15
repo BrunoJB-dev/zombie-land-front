49f960174ec3a5c51964337776c4d93389b65c02
Mickaël-Goua <micke.goua@gmail.com>
2024-11-15 09:12:44 +0100

    end a propos page

--- src/pages/APropos/APropos.tsx
+++ src/pages/APropos/APropos.tsx
@@ -12,9 +12,7 @@ function APropos() {
           <p>Avec 20 attractions uniques, le parc propose une gamme d’expériences adaptées aux amateurs de sensations fortes comme aux petits aventuriers. Venez défier la gravité sur le Zombie Escape Coaster, explorez les profondeurs du Labyrinthe Mortel, ou embarquez pour une mission en réalité virtuelle dans Virus Outbreak VR ! Pour les courageux, notre Apocalypse Karting vous offre une course déjantée à travers un paysage de ruines, tandis que le Safari Zombie vous emmène en tour de jeep au plus près des créatures… si vous osez !</p>
           <p>Que vous soyez ici pour affronter vos pires cauchemars ou simplement pour vous amuser avec un brin d’adrénaline, Zombieland est bien plus qu’un parc : c’est une aventure inoubliable qui vous poussera à défier vos limites dans une ambiance de fin du monde. Prêts pour l’aventure de votre vie ? Les zombies d’Hébécrevon n’attendent que vous ! 🧟</p>
         </article>
-        <div className="img-apropos">
-          <img src={parc} alt="le parc" />
-        </div>
+          <img className="img-apropos" src={parc} alt="le parc" />
       </div>
     </div>
   )
